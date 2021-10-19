2daec5a64c993722e6ae2c5597027a9349977b19
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-19 19:56:29 +0800

    [PosMachine][Benny] Feat: add generation of array with only distinct from other array

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,22 @@
 package pos.machine;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+
         return null;
+    }
+
+    public String[] generateDistinctItemList(String[] barcodeList){
+        List<String> outputList = new ArrayList<>();
+        for (String barcode : barcodeList){
+            if (!java.util.Arrays.stream(outputList.toArray()).anyMatch(barcode::equals)){
+                outputList.add(barcode);
+            }
+        }
+        return (String[])outputList.toArray();
     }
 }
